65144033412edea126037c5ab535e40649b81b21
Marcin Jeleński <marcin@jelenski.net>
2021-11-29 20:40:31 +0100

    Fix import

--- test/paint/splash_test.dart
+++ test/paint/splash_test.dart
@@ -2,7 +2,6 @@ import 'dart:io';
 
 import 'package:ditredi/ditredi.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:stack_trace/stack_trace.dart' as stacktrace;
 
@@ -12,17 +11,17 @@ void main() {
   diTreDiDrawTest('splash.png', (tester, controller) async {
     controller.update(
       userScale: 10,
-      rotationX: -30,
+      rotationX: -20,
       rotationY: 30,
-      translation: const Offset(30, -130),
+      translation: const Offset(80, -0),
     );
     final mesh = await _getTerrainCsv();
-    await tester.pumpWidget(Container(
-      color: Colors.black,
-      child: DiTreDi(
-        figures: [...mesh],
-        controller: controller,
-        config: const DiTreDiConfig(defaultPointWidth: 2),
+    await tester.pumpWidget(DiTreDi(
+      figures: [...mesh],
+      controller: controller,
+      config: DiTreDiConfig(
+        defaultPointWidth: 2,
+        defaultColorPoints: Colors.grey.shade500,
       ),
     ));
   }, width: 776, height: 300);
